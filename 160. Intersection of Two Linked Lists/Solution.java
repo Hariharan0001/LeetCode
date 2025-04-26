c231713967110f9de955aa2fbdfa86c356bb69e6
HARI <103643955+Hariharan0001@users.noreply.github.com>
2025-04-26 22:44:49 +0530

    Java Solution
          Submission Statistics:
          Question #: 160
          Language: Java
          Runtime: 1 ms
          Runtime percentile: 99.93
          Memory: 48.632 MB
          Memory percentile: 57.14

--- 160. Intersection of Two Linked Lists/Solution.java
+++ 160. Intersection of Two Linked Lists/Solution.java
@@ -14,38 +14,10 @@ public class Solution {
         if(headA==null || headB==null) return null;
         ListNode temp1 = headA;
         ListNode temp2 = headB;
-        int alen = 0;
-        int blen = 0;
-        while(temp1!=null){
-            alen+=1;
-            temp1 = temp1.next;
+        while(temp1!=temp2){
+            temp1 = (temp1 == null) ? headB : temp1.next;
+            temp2 = (temp2 == null) ? headA : temp2.next;
         }
-        while(temp2!=null){
-            blen+=1;
-            temp2 = temp2.next;
-        }
-        int diff = 0;
-        if(alen<blen){
-            diff  = blen-alen;
-            while(diff!=0){
-                headB = headB.next; 
-                diff--;
-            }
-        }
-        else{
-            diff = alen-blen;
-            while(diff!=0){
-                headA = headA.next;
-                diff--;
-            }
-        }
-        while(headA!=null){
-            if(headA==headB){
-                return headA;
-            }
-            headA = headA.next;
-            headB = headB.next;
-        }
-        return null;
+        return temp1;
     }
 }
