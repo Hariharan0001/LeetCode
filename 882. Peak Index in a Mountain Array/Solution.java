f52262ba067e27814d9200306a1dfb3a994dee35
HARI <103643955+Hariharan0001@users.noreply.github.com>
2025-05-26 23:00:41 +0530

    Java Solution
          Submission Statistics:
          Question #: 882
          Language: Java
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 56.208 MB
          Memory percentile: 59.50

--- 882. Peak Index in a Mountain Array/Solution.java
+++ 882. Peak Index in a Mountain Array/Solution.java
@@ -5,17 +5,15 @@ class Solution {
         int l = 0;
         int r = n-1;
         while(l<r){
-            
+            mid = l + ((r-l)/2);
             if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid]){
                 return mid;
             }
             if(arr[mid]<arr[mid+1]){
                 l = mid+1;
-                mid = l + ((r-l)/2);
             }
             else{
                 r = mid;
-                mid = l + ((r-l)/2);
             }
         }  
         return -1;
