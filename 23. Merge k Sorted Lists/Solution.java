f3893681ceeaebe0fba2b1d011ab9d5683296ac8
HARI <103643955+Hariharan0001@users.noreply.github.com>
2025-06-20 22:47:04 +0530

    Java Solution
          Submission Statistics:
          Question #: 23
          Language: Java
          Runtime: 4 ms
          Runtime percentile: 76.23
          Memory: 44.22 MB
          Memory percentile: 89.85

--- 23. Merge k Sorted Lists/Solution.java
+++ 23. Merge k Sorted Lists/Solution.java
@@ -9,48 +9,65 @@
  * }
  */
 class Solution {
-    public ListNode mergetwoList(ListNode list1, ListNode list2){
-        if(list1==null && list2!=null) return list2;
-        if(list2==null && list1!=null) return list1;
-        if(list1==null && list2==null) return null;
-        ListNode head = null;
-        ListNode tail = null;
-        if(list1.val<=list2.val){
-            head = tail = list1;
-            list1 = list1.next;
-        }
-        else{
-            head = tail = list2;
-            list2 = list2.next;
-        }
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){
-                tail.next = list1;
-                tail = list1;
-                list1 = list1.next;
-            }
-            else{
-                tail.next = list2;
-                tail = list2;
-                list2 = list2.next;
-            }
-        }
-        if(list1!=null){
-            tail.next = list1;
-        }
-        else{
-            tail.next = list2;
-        }
-        return head;
-    }
+    // public ListNode mergetwoList(ListNode list1, ListNode list2){
+    //     if(list1==null && list2!=null) return list2;
+    //     if(list2==null && list1!=null) return list1;
+    //     if(list1==null && list2==null) return null;
+    //     ListNode head = null;
+    //     ListNode tail = null;
+    //     if(list1.val<=list2.val){
+    //         head = tail = list1;
+    //         list1 = list1.next;
+    //     }
+    //     else{
+    //         head = tail = list2;
+    //         list2 = list2.next;
+    //     }
+    //     while(list1!=null && list2!=null){
+    //         if(list1.val<=list2.val){
+    //             tail.next = list1;
+    //             tail = list1;
+    //             list1 = list1.next;
+    //         }
+    //         else{
+    //             tail.next = list2;
+    //             tail = list2;
+    //             list2 = list2.next;
+    //         }
+    //     }
+    //     if(list1!=null){
+    //         tail.next = list1;
+    //     }
+    //     else{
+    //         tail.next = list2;
+    //     }
+    //     return head;
+    // }
     public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        if(n==0) return null;
-        if(n==1) return lists[0];
-        ListNode ans = lists[0];
-        for(int i=1;i<n;i++){
-           ans = mergetwoList(ans, lists[i]);
+        // int n = lists.length;
+        // if(n==0) return null;
+        // if(n==1) return lists[0];
+        // ListNode ans = lists[0];
+        // for(int i=1;i<n;i++){
+        //    ans = mergetwoList(ans, lists[i]);
+        // }
+        // return ans;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((a,b)-> a.val - b.val);
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        for(int i=0;i<lists.length;i++){
+            if(lists[i]!=null){
+                queue.offer(lists[i]);
+            }
         }
-        return ans;
+        while(!queue.isEmpty()){
+            ListNode temp = queue.poll();
+            current.next = temp;
+            current = current.next;
+            if(temp.next!=null){
+                queue.offer(temp.next);
+            }
+        }
+        return dummy.next;
     }
 }
